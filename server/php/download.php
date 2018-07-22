f1b11c389c19c6974c67a65176975336b5b24533
e7d <michael@e7d.io>
2018-07-22 11:46:05 +0200

    Improve PHP download route performance

--- server/php/download.php
+++ server/php/download.php
@@ -3,14 +3,15 @@
 // remove max execution time
 set_time_limit(0);
 
-// Read input
-$chunkSize = 1 * 1024 * 1024;
-$size = intval($_GET['size'] ?? 0);
-
 // Disable Compression
 @ini_set('zlib.output_compression', 'Off');
 @ini_set('output_buffering', 'Off');
 @ini_set('output_handler', '');
+
+// Read input
+$size = $_GET['size'] ?? INF;
+$chunkSize = $_GET['chunkSize'] ?? 64 * 1024;
+$chunks = $size / $chunkSize;
 
 // Headers
 header('HTTP/1.1 200 OK');
@@ -20,7 +21,7 @@ header('Content-Description: File Transfer');
 header('Content-Type: application/octet-stream');
 header('Content-Disposition: attachment; filename=random.dat');
 header('Content-Transfer-Encoding: binary');
-if ($size > 0) {
+if ($size !== INF) {
     header('Content-Length: ' . $size);
 }
 
@@ -29,14 +30,11 @@ header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
 header('Cache-Control: post-check=0, pre-check=0', false);
 header('Pragma: no-cache');
 
-// Generate 1M chunks of random data
-$data = openssl_random_pseudo_bytes($chunkSize);
+// Generate 1M chunk of random data
+$data = random_bytes($chunkSize);
 
 // Assemble enough chunks to reach required size
-if ($size === 0) {
-    $size = INF;
-}
-for ($i = 0; $i < $size; $i++) {
-    echo $data;
+for ($i = 0; $i < $chunks; $i++) {
+    print $data;
     flush();
 }
