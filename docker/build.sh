c9098b627113e248620694f8c055a58d016f255f
e7d <michael@e7d.io>
2018-10-16 10:34:10 +0200

    Add a root Dockerfile

--- docker/build.sh
+++ docker/build.sh
@@ -2,7 +2,7 @@
 
 dockerBuild()
 {
-    docker build --no-cache -t e7db/speedtest:$1 -f $PWD/docker/$1.Dockerfile $PWD
+    docker build --no-cache -t e7db/speedtest:$2 -f $PWD/docker/$1.Dockerfile $PWD
 }
 
 webBuild()
@@ -12,15 +12,23 @@ webBuild()
 
 cd `dirname $0`/..
 cmd=$1
+tag=$2
+if [ -z $tag ]; then
+    tag=$cmd
+fi
 while true; do
     case $cmd in
-        "go") dockerBuild go; break;;
-        "node") dockerBuild node; break;;
-        "php") dockerBuild php; break;;
-        "python") dockerBuild python; break;;
-        "ruby") dockerBuild ruby; break;;
-        "web") webBuild; break;;
-        * ) echo "Please answer \"go\", \"node\", \"php\", \"python\", \"ruby\" or \"web\".";;
+        "go"|"node"|"php"|"python"|"ruby")
+            dockerBuild $cmd $tag
+            break
+            ;;
+        "web")
+            webBuild
+            break
+            ;;
+        * )
+            echo "Please answer \"go\", \"node\", \"php\", \"python\", \"ruby\" or \"web\"."
+            ;;
     esac
     read -p "What to build? " cmd
 done
