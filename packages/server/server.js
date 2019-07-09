f5b435b268e28105a0b886c9a0cc5c7d87207b26
e7d <michael@e7d.io>
2019-07-09 15:32:48 +0200

    fix env vars reading

--- packages/server/server.js
+++ packages/server/server.js
@@ -4,12 +4,12 @@ const path = require("path");
 const Logger = require("./src/logger");
 const HttpServer = require("./src/httpServer");
 
-const basePath = process.env['BASE_PATH'] || "web";
-const httpPort = process.env['HTTP_PORT'] || 80;
-const httpsPort = process.env['HTTPS_PORT'] || 443;
-const key = process.env['KEY'] || "certificates/localhost.key";
-const cert = process.env['CERT'] || "certificates/localhost.pem";
-const ca = process.env['CA'] || undefined;
+const basePath = process.env.BASE_PATH || "web";
+const httpPort = process.env.HTTP_PORT || 80;
+const httpsPort = process.env.HTTPS_PORT || 443;
+const key = process.env.KEY || "certificates/localhost.key";
+const cert = process.env.CERT || "certificates/localhost.pem";
+const ca = process.env.CA || undefined;
 const logger = new Logger();
 
 if (!fs.existsSync(path.join(process.cwd(), "results"))) {
