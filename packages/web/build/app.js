b36287c25fbd227dce5cc8db656ab3766ed410c6
e7d <michael@e7d.io>
2019-09-04 12:09:36 +0200

    update packages, improve env setup and build

--- packages/web/build/app.js
+++ packages/web/build/app.js
@@ -1,5 +1,7 @@
 import "../src/css/gauge.scss";
 import "../src/css/app.scss";
 
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import App from "../src/js/app.js";
 new App();
--- packages/web/build/serviceWorker.js
+++ packages/web/build/serviceWorker.js
@@ -1,1 +1,3 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import "../src/js/serviceWorker";
